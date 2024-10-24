02ba085d1e8f696079fb43e7691e92cb97f43523
vale09 <m.valerio9109@gmail.com>
2024-10-24 14:49:19 +0200

    add certs and frontend domain for cors

--- src/main/java/net/myself/DemoLibrary/Infrastructure/Configuration/SecurityConf.java
+++ src/main/java/net/myself/DemoLibrary/Infrastructure/Configuration/SecurityConf.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +13,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -22,16 +25,31 @@ import java.util.List;
 public class SecurityConf{
 	@Value("${auth0.domain}")
 	private String domain;
+	@Value("${frontend.domain}")
+	private String frontEndDomain;
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
+						.cors(Customizer.withDefaults())
 						.authorizeHttpRequests((authz) -> authz
 										.requestMatchers("/authentication/login").permitAll()
 										.anyRequest().authenticated())
 						.csrf(AbstractHttpConfigurer::disable)
 						.oauth2ResourceServer(oauth2 -> oauth2.jwt((conv) -> conv.jwtAuthenticationConverter(jwtAuthenticationConverter())));
 		return http.build();
+	}
+	@Bean
+	WebMvcConfigurer corsConfigurer()
+	{
+		return new WebMvcConfigurer()
+		{
+			@Override
+			public void addCorsMappings(CorsRegistry registry)
+			{
+				registry.addMapping("/**").allowedOrigins(frontEndDomain);
+			}
+		};
 	}
 	
 	private JwtAuthenticationConverter jwtAuthenticationConverter() {
